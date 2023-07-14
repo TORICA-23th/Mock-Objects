ad7527c3b4a073252158e1a9645d9cbbae1f36e3
Syo-121 <131446710+Syo-121@users.noreply.github.com>
2023-07-14 21:17:21 +0900

    Update RPmock.ino

--- RPmock/RPmock.ino
+++ RPmock/RPmock.ino
@@ -10,20 +10,24 @@
 
 char sendUART_BUF[256];
 
-volatile float air_dps_altitude_m = 0;
-volatile float air_dps_pressure_hPa = 0;
-volatile float air_dps_temperature_deg = 0;
-volatile float air_sdp_airspeed_ms = 0;
-volatile float air_sdp_differentialPressure_Pa = 0;
-volatile float air_sdp_airspeed_mss = 0;
-volatile float air_sdp_temperature_deg = 0;
+volatile float air_dps_altitude_m = 71;
+volatile float air_dps_pressure_hPa = 1013.0;
+volatile float air_dps_temperature_deg = 30;
+volatile float air_sdp_airspeed_ms = 5.0;
+volatile float air_sdp_differentialPressure_Pa = 1013.0;
+volatile float air_sdp_temperature_deg = 30;
 
-volatile float under_dps_pressure_hPa = 0;
-volatile float under_dps_temperature_deg = 0;
-volatile float under_dps_altitude_m = 0;
-volatile float under_urm_altitude_m = 0;
+volatile float under_dps_pressure_hPa = 1013.0;
+volatile float under_dps_temperature_deg = 30;
+volatile float under_dps_altitude_m = 70;
+volatile float under_urm_altitude_m = 0.5;
+
+volatile float true_m = 10.5;
 
 unsigned long int last_send_time = 0;
+
+int loop_count = 0;
+int flag = true;
 
 void setup() {
   // put your setup code here, to run once:
@@ -35,15 +39,29 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (millis() - last_send_time>40) {
     last_send_time = millis();
-    
-    //under
-    //気圧[hPa],温度[deg],気圧高度[m],超音波高度[m]
-    sprintf(sendUART_BUF, "%.2f,%.2f,%.2f,%.2f\n", under_dps_pressure_hPa, under_dps_temperature_deg, under_dps_altitude_m, under_urm_altitude_m);
-    SerialOUT.print(sendUART_BUF);
-
-    //air
-    //気圧[hPa],温度[deg],気圧高度[m],差圧[Pa],対気速度[m/ss]  
-    sprintf(sendUART_BUF, "%.2f,%.2f,%.2f,%.2f,%.2f\n", air_dps_pressure_hPa, air_dps_temperature_deg, air_dps_altitude_m, air_sdp_differentialPressure_Pa, air_sdp_airspeed_ms);
-    SerialOUT.print(sendUART_BUF);
+    if(millis() < 15000){
+      under_urm_altitude_m = 0.5;
+    }
+    else{            
+        true_m -= 0.0067;
+        air_dps_altitude_m -= 0.0072;
+        under_dps_altitude_m -= 0.0072;
+  
+        if(true_m <= 5.0){
+          under_urm_altitude_m = true_m;
+        }else{
+          under_urm_altitude_m = 10.0;
+        }
+    }
+  
+      //under
+      //気圧[hPa],温度[deg],気圧高度[m],超音波高度[m]
+      sprintf(sendUART_BUF, "%.2f,%.2f,%.2f,%.2f\n", under_dps_pressure_hPa, under_dps_temperature_deg, under_dps_altitude_m, under_urm_altitude_m);
+      Serial1.print(sendUART_BUF);
+  
+      //air
+      //気圧[hPa],温度[deg],気圧高度[m],差圧[Pa],対気速度[m/ss]  
+      sprintf(sendUART_BUF, "%.2f,%.2f,%.2f,%.2f,%.2f\n", air_dps_pressure_hPa, air_dps_temperature_deg, air_dps_altitude_m, air_sdp_differentialPressure_Pa, air_sdp_airspeed_ms);
+      Serial2.print(sendUART_BUF);
   }
 }
