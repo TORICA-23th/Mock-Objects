d5b350303eb7356c228f88efada79c1f2fe32c61
771-8bit <kazuki.fujita8823@gmail.com>
2023-07-22 23:38:55 +0900

    flag_landed

--- RPmock/RPmock.ino
+++ RPmock/RPmock.ino
@@ -22,11 +22,11 @@ volatile float under_dps_temperature_deg = 30;
 volatile float under_dps_altitude_m = 70;
 volatile float under_urm_altitude_m = 0.5;
 
-volatile float true_m = 10.5;
+volatile float true_m = 10.6;
 unsigned long int last_send_time = 0;
 
 int loop_count = 0;
-int flag = false;
+int flag_landed = false;
 
 //乱数生成
 float randNumber;
@@ -42,45 +42,40 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (millis() - last_send_time>40) {
+  if (millis() - last_send_time > 40) {
     last_send_time = millis();
-    
+
     //乱数生成
-    randNumber = random(92, 112); 
+    randNumber = random(92, 112);
     air_sdp_airspeed_ms = randNumber / 10;
 
     Serial.println(air_sdp_airspeed_ms);
-    
-    if(millis() < 15000){
-      under_urm_altitude_m = 0.5;
+
+    if (millis() < 15000) {
+      under_urm_altitude_m = 0.6;
+    } else if (!flag_landed) {
+      true_m -= 0.006799;
+      air_dps_altitude_m -= 0.006800;
+      under_dps_altitude_m -= 0.006800;
+    } else {
+      if (true_m <= 0.006799) {
+        flag_landed = true;
+      }
+      if (true_m <= 5.0) {
+        under_urm_altitude_m = true_m;
+      } else {
+        under_urm_altitude_m = 10.0;
+      }
     }
-    else if(flag){
-      air_dps_altitude_m = 0;
-      under_dps_altitude_m = 0;
-      under_urm_altitude_m = 0;
-    }
-    else{            
-        true_m -= 0.006799;
-        air_dps_altitude_m -= 0.006800;
-        under_dps_altitude_m -= 0.006800;
-        if(true_m <= 0.006799){
-          flag = true;
-        }
-        if(true_m <= 5.0){
-          under_urm_altitude_m = true_m;
-        }else{
-          under_urm_altitude_m = 10.0;
-        }
-    }
-  
-      //under
-      //気圧[hPa],温度[deg],気圧高度[m],超音波高度[m]
-      sprintf(sendUART_BUF, "%.2f,%.2f,%.2f,%.2f\n", under_dps_pressure_hPa, under_dps_temperature_deg, under_dps_altitude_m, under_urm_altitude_m);
-      Serial1.print(sendUART_BUF);
-  
-      //air
-      //気圧[hPa],温度[deg],気圧高度[m],差圧[Pa],対気速度[m/ss]  
-      sprintf(sendUART_BUF, "%.2f,%.2f,%.2f,%.2f,%.2f\n", air_dps_pressure_hPa, air_dps_temperature_deg, air_dps_altitude_m, air_sdp_differentialPressure_Pa, air_sdp_airspeed_ms);
-      Serial2.print(sendUART_BUF);
+
+    //under
+    //気圧[hPa],温度[deg],気圧高度[m],超音波高度[m]
+    sprintf(sendUART_BUF, "%.2f,%.2f,%.2f,%.2f\n", under_dps_pressure_hPa, under_dps_temperature_deg, under_dps_altitude_m, under_urm_altitude_m);
+    Serial1.print(sendUART_BUF);
+
+    //air
+    //気圧[hPa],温度[deg],気圧高度[m],差圧[Pa],対気速度[m/ss]
+    sprintf(sendUART_BUF, "%.2f,%.2f,%.2f,%.2f,%.2f\n", air_dps_pressure_hPa, air_dps_temperature_deg, air_dps_altitude_m, air_sdp_differentialPressure_Pa, air_sdp_airspeed_ms);
+    Serial2.print(sendUART_BUF);
   }
 }
